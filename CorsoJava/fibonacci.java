c8f8f6550fea42bc8fb2b02dabfa8957bf8bbd37
Nicolò Tola <njkoplay@gmail.com>
2023-02-20 12:23:10 +0100

    cambio nel ciclo

--- CorsoJava/fibonacci.java
+++ CorsoJava/fibonacci.java
@@ -5,10 +5,10 @@ public class fibonacci {
         Scanner oggetto = new Scanner(System.in);
         int numero, n2 = 1, n1 = 0 ,n = 0;
 
-        System.out.println("inserisci fino a quanto vuoi arrivare: ");
+        System.out.println("inserisci quanti numeri della sequenza vuoi visualizzare: ");
         numero = oggetto.nextInt();
 
-        while(n <= numero){
+        for(int i = 0; i<numero; i++){
             n = n1+n2;
             n1 = n2;
             n2 = n;
