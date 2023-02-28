7b0fa610752058d23b250058f13a7b793d91e74f
Nicolò Tola <njkoplay@gmail.com>
2023-02-28 17:16:46 +0100

    esercizio costruttori

---
+++ CorsoJava/costruttore.java
@@ -0,0 +1,21 @@
+public class costruttore {
+    int anno;
+    String nome, sala;
+    public costruttore(String nomeFilm, int annoProd){
+        nome = nomeFilm;
+        anno = annoProd;
+    }
+
+    public costruttore(String nomeFilm, int annoProd, String salaPrem){
+        nome = nomeFilm;
+        anno = annoProd;
+        sala = salaPrem;
+    }
+
+    public static void main(String[] args){
+        costruttore film1 = new costruttore("sos", 2001);
+        costruttore film2 = new costruttore("sos2", 2002, "sala1");
+        System.out.println(film1.nome + " " + film1.anno);
+        System.out.println(film2.nome + " " + film2.anno + " " + film2.sala);
+    }
+}
