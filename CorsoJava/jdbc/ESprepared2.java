5a742fe326d4bba6fd61228e02ba1e970384629f
Nicolò Tola <njkoplay@gmail.com>
2023-03-02 10:16:28 +0100

    Esercizi statement finiti

--- CorsoJava/jdbc/ESprepared2.java
+++ CorsoJava/jdbc/ESprepared2.java
@@ -1,41 +1,71 @@
+import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-public class ESprepared2 {
+
+public class ESprepared2{ //da finire con: scegliere se mostrare il nome della nazione a cui fa riferimento il code
     public static void main(String[] args){    
-        /* 
-        * aggiungi fibonacci
-        */
+        Scanner ogg = new Scanner(System.in); //string
+        Scanner ogg1 = new Scanner(System.in); //int
+        int x;
+        String scelta, sceltaOrd = null;
         String url = "jdbc:mysql://localhost:3306/world";
+        String dbUserName = "root";
+        String password = "root";
         //String DB_DRIVER = "com.mysql.jdbc.Driver" ovvero il connector
     
-        Connection myConnection = null;
+       
 
         try {
-            myConnection = DriverManager.getConnection(url, "root", "root");
+            Connection myConnection = DriverManager.getConnection(url, dbUserName, password);
             if(myConnection == null){
                 System.out.print("connessione non stabilita");
             }
             else{
+                
                 System.out.println("connessione stabilita");
-            
-                String query = String.format("select country.Name," + 
-                " case when country.Population > 100000 then 'la superficie è maggiore di 100.000' when country.Population < 100000 then ' la superficie è minore di 100.000' else 'la superficie è 100.000' end," + 
-                " ifnull(country.IndepYear, 'nessun indepyear') from country;");
-            
-                Statement stm = myConnection.createStatement();
-                ResultSet rs=stm.executeQuery(query);
+                
+                System.out.println("Vuoi visualizzare in ordine 1 crescente o 2 decrescente?: ");
+                x = ogg1.nextInt();
+                switch(x){
+                    case 1:
+                        sceltaOrd = "asc";
+                        break;
+                    case 2: 
+                        sceltaOrd = "desc";
+                        break;
+                    default:
+                        System.out.println("input errato addio");
+                        System.exit(0);
+                }
+
+                String query = String.format("SELECT city.Name, city.Population, city.CountryCode" +
+                " FROM world.city" + 
+                " WHERE city.CountryCode = ? AND city.Population >= ? " +
+                "ORDER BY city.Name " + sceltaOrd + " ;");
+                
+                PreparedStatement stm = myConnection.prepareStatement(query);
+                
+                System.out.println("Inserisci il countryCode: ");
+                scelta = ogg.nextLine();
+                stm.setString(1, scelta);
+                
+                System.out.println("Inserisci la popolazione minima: ");
+                x = ogg1.nextInt();
+                stm.setInt(2, x);
+
+                ResultSet rs = stm.executeQuery();
 
                 while(rs.next()){
-                    String tableFormat = String.format("Name: %s | Superficie: %s | IndepYear: %s", rs.getString(1),
-                                                                                        rs.getString(2),
-                                                                                                rs.getString(3));
+                    String tableFormat = String.format("Name: %s | Popolazione: %s | CountryCode: %s", rs.getString(1),
+                                                                                                    rs.getString(2),
+                                                                                                    rs.getString(3));
                     System.out.println(tableFormat + "\n");
                 }
             }
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- CorsoJava/jdbc/preparedStatement.java
+++ CorsoJava/jdbc/preparedStatement.java
@@ -2,23 +2,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-
-import com.mysql.cj.jdbc.Driver;
 
 public class preparedStatement{
-    public static void main(String[] args){    
-        /* 
-        * NON FUNZIONA
-        */
+    public static void main(String[] args){
+
         String url = "jdbc:mysql://localhost:3306/world";
+        String dbUserName = "root";
+        String password = "root";
         //String DB_DRIVER = "com.mysql.jdbc.Driver" ovvero il connector
     
        
 
         try {
-            Connection myConnection = DriverManager.getConnection(url, "root", "root");
-            myConnection = DriverManager.getConnection(url, "root", "root");
+            Connection myConnection = DriverManager.getConnection(url, dbUserName, password);
             if(myConnection == null){
                 System.out.print("connessione non stabilita");
             }
@@ -27,10 +23,12 @@ public class preparedStatement{
                 String query = String.format("select country.Name," + 
                 " case when country.Population > ? then 'la superficie è maggiore di 100.000' when country.Population < ? then ' la superficie è minore di 100.000' else 'la superficie è 100.000' end," + 
                 " ifnull(country.IndepYear, 'nessun indepyear') from country;");
+                
                 PreparedStatement stm = myConnection.prepareStatement(query);
                 stm.setInt(1, 100000);
+                stm.setInt(2, 100000);
 
-                ResultSet rs=stm.executeQuery(query);
+                ResultSet rs = stm.executeQuery();
 
                 while(rs.next()){
                     String tableFormat = String.format("Name: %s | Superficie: %s | IndepYear: %s", rs.getString(1),
