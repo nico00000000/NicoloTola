6ff13e8ab630eaa37e09b94dc37d1736321f95aa
Nicolò Tola <njkoplay@gmail.com>
2023-03-06 13:02:10 +0100

    ciclo menu

--- CorsoJava/Fibonacci/menuFibonacci.java
+++ CorsoJava/Fibonacci/menuFibonacci.java
@@ -7,25 +7,42 @@ public class menuFibonacci {
         Scanner oggetto1 = new Scanner(System.in);
         MetodiFibonacci ogg1 = new MetodiFibonacci();
         int s,num;
-        System.out.println("Inserisci 1 per stampare 2 per cercare un numero e 3 per uscire: ");
-        s = oggetto.nextInt();
-        switch(s){
-            case 1:
-                System.out.println("inserisci quanti numeri della sequenza vuoi visualizzare: ");
-                num = oggetto1.nextInt();
-                ogg1.Stampanum(num);
-                break;
-            case 2:
-                System.out.println("inserisci quale numero vuoi trovare: ");
-                num = oggetto1.nextInt();
-                ogg1.Cercanum(num);
-                break;
-            case 3:
+        boolean ciclo = true;
+        while(ciclo == true){    
+            System.out.println("Inserisci 1 per stampare 2 per cercare un numero e 3 per uscire: ");
+            s = oggetto.nextInt();
+            switch(s){
+                case 1:
+                    System.out.println("inserisci quanti numeri della sequenza vuoi visualizzare: ");
+                    num = oggetto1.nextInt();
+                    ogg1.Stampanum(num);
+                    break;
+                case 2:
+                    System.out.println("inserisci quale numero vuoi trovare: ");
+                    num = oggetto1.nextInt();
+                    ogg1.Cercanum(num);
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("input errato riprova");
+                    System.exit(0);
+            }
+            System.out.println("vuoi continuare?: 1 si 2 no ");
+            s = oggetto.nextInt();
+            if(s == 1 || s == 2){
+                if(s == 1){
+                    ciclo = true;
+                }else if(s == 2){
+                    ciclo = false; 
+                }
+            }
+            else{
+                System.out.println("input errato");
                 System.exit(0);
-            default:
-                System.out.println("input errato riprova");
-                Menu();
+            }
         }
+
         oggetto.close();
         oggetto1.close();
     }
