b18cdadba4c4fe060db3bf5bba39c483faa6b4fb
Nicolò Tola <njkoplay@gmail.com>
2023-03-07 15:11:32 +0100

    overriding

---
+++ overriding/Test2.java
@@ -0,0 +1,35 @@
+package overriding;
+class Bank{
+    int getRateOfInterest(){
+        return 0;
+    }
+}
+
+class SBI extends Bank{
+    int getRateOfInterest(){
+        return 8;
+    }
+}
+
+class ICICI extends Bank{
+    int getRateOfInterest(){
+        return 7;
+    }
+}
+
+class AXIS extends Bank{
+    int getRateOfInterest(){
+        return 9;
+    }
+}
+
+class Test2{
+    public static void main(String[] args){
+        SBI s = new SBI();
+        ICICI i = new ICICI();
+        AXIS a = new AXIS();
+        System.out.println("SBI interessi: " + s.getRateOfInterest());
+        System.out.println("ICICI interessi: " + i.getRateOfInterest());
+        System.out.println("AXIS interessi: " + a.getRateOfInterest());
+    }
+}
