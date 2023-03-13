071b12fc44898307aa739aa2b2d78ca33e11bed2
Nicolò Tola <njkoplay@gmail.com>
2023-03-13 11:56:49 +0100

    d

--- Test1/es1.java
+++ Test1/es1.java
@@ -4,8 +4,10 @@ public class es1 {
     
     public static void main(String[] args){
         metodiDB crea = new metodiDB();
+        menuDB mymenu = new menuDB();
         crea.creaDataB();
         crea.inserTabelle();
+        mymenu.menu();
     }
 }
 
